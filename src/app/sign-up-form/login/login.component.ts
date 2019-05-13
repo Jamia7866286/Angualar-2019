f76c09fadcc4215c53e98337cee62ad55ba8dcc1
Naseem Ahmad <naseemahmadjmi96@gmail.com>
2019-05-13 17:22:48 +0530

    From office

--- src/app/sign-up-form/login/login.component.ts
+++ src/app/sign-up-form/login/login.component.ts
@@ -1,21 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-import { ok } from 'assert';
-import { hasLifecycleHook } from '@angular/compiler/src/lifecycle_reflector';
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core"
+import { FormGroup, FormBuilder, Validators, Form } from "@angular/forms"
+import Swal from "sweetalert2"
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.less']
+   selector: "app-login",
+   templateUrl: "./login.component.html",
+   styleUrls: ["./login.component.less"],
 })
 export class LoginComponent implements OnInit {
+   loginForm: FormGroup
+   @Input() public parentData
+   constructor(formbuilderlogin: FormBuilder) {
+      this.loginForm = formbuilderlogin.group({
+         mail: ["", [Validators.required, Validators.email]],
+         pswd: ["", [Validators.required, Validators.minLength(4)]],
+      })
+   }
 
-  constructor() { }
+   get logincontent() {
+      return this.loginForm.controls
+   }
+   @Output() public childEvent = new EventEmitter()
 
-  ngOnInit() {
-  }
+   ngOnInit() {}
 
-  register(e){
-    
-  }
+   register() {
+      this.parentData = false
+      this.childEvent.emit(this.parentData)
+   }
 
+   LogInFormdata() {
+      Swal.fire({
+         position: "center",
+         type: "success",
+         title: "Thanks for login...",
+         showConfirmButton: false,
+         timer: 1300,
+      })
+   }
 }
