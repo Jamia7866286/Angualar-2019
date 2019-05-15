9c087af114cd5eae21efe123bf2f7dcdc4063689
Naseem Ahmad <naseemahmadjmi96@gmail.com>
2019-05-15 09:56:01 +0530

    From office

--- src/app/weather-app/weather-app.component.ts
+++ src/app/weather-app/weather-app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from "@angular/core"
 import { WeatherService } from "../weather.service"
-import Swal from "sweetalert2"
-
+// import Swal from "sweetalert2"
 @Component({
    selector: "app-weather-app",
    templateUrl: "./weather-app.component.html",
@@ -11,41 +10,32 @@ export class WeatherAppComponent {
    constructor(private getdataaccess: WeatherService) {}
    weatherInput: any
    datacontentShow = false
-   // mydt_txt
    datafetch: any
    country_flag: any
    city_name: any
-   loader = false;
-   arr: any = []
+   loader = true;
+   DateArray: any = []
+   TimeArray: any = []
    submitData(pin: any) {
-      this.loader = true;
       if (pin) {
          this.getdataaccess.getdataServer(pin).subscribe(
             (data: any) => {
-               this.loader = false
+               
                this.datacontentShow = true
                console.log(data)
                this.country_flag = data.city.country
                this.city_name = data.city.name
                this.datafetch = data.list
-               console.log(this.datafetch)
 
-               // let s = new Date(i.dt_txt).toLocaleTimeString()
-               data.list.map((val, i) => {
-                  this.arr.push(new Date(data.list[i].dt).toLocaleTimeString())
-               })
-               console.log(this.arr)
+                data.list.map((val: any, index: any) => {
+                   this.DateArray.push(new Date(data.list[index].dt_txt).toLocaleDateString());
+                })
 
-               //    this.datafetch = Object.keys(data.list).map((val,index)=>{
-               //    this.mydt_txt = data.list[index].dt_txt;
-               //    var initial = this.mydt_txt.split(' ');
-               //     return initial[0] + "<br>" + initial[1]
-               // });
-            },
-            error => {
-               Swal.fire({
-                  type: "success",
-               })
+                data.list.map((val: any,index: any)=>{
+                  this.TimeArray.push(new Date(data.list[index].dt_txt).toLocaleTimeString());
+                });
+
+               this.loader = false
             }
          )
       } else {
