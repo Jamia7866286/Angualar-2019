e59528d9f6b14389714b21d497215c08fcfbe50e
Naseem Ahmad <naseemahmadjmi96@gmail.com>
2019-05-22 11:02:36 +0530

    From office

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-// import * as $ from 'jquery';
 @Component({
   selector: 'app-calculator',
   templateUrl: './calculator.component.html',
@@ -39,63 +38,88 @@ div(e){
  this.result = this.num1 /  this.num2;
  this.operation = 'Divisions';
 }
-
   
-// New calculator blinking js
-// ngOnInit(){
-//     $(document).ready(() => {
-// 		setInterval(function(){
-// 			$('.blink-me').fadeOut(200);
-// 			$('.blink-me').fadeIn(200);
-// 		});
-//     });
-//   }
+// resultcondition = false;
+// infix_string = [];
+// resultcondition_zero = false
+// number_calculation:any = [2]
+// plus_result;
+// number_1 = 0
+// number_2
+// number_plus
+// number1_event(val:any){
+	// this.resultcondition_zero = true;
+	// this.infix_string.push(val)
+	// console.log(this.infix_string);
+	// this.number_calculation[0] = val
+  //   console.log(this.number_calculation[0]);
+// }
+// flag = 0
 
-resultcondition = false;
-infix_string = [];
-resultcondition_zero = false
-number_calculation:any = [2]
-plus_result;
-number_1 = 0
-number_2
-number_plus
-number1_event(val:any){
-	this.resultcondition_zero = true;
-	this.infix_string.push(val)
-	console.log(this.infix_string);
-	this.number_calculation[0] = val
-    console.log(this.number_calculation[0]);
-}
-
-number2_event(val:any){
-	this.infix_string.push(val)
-	this.number_calculation[2] = val;
-	this.resultcondition = true;
-	console.log(this.number_calculation[2]);
-	if(this.number_calculation[1] === '+'){
-		this.plus_result = this.number_calculation[0] + this.number_calculation[2];
-	}
-	else if(this.number_calculation[1] === '-'){
-		this.plus_result = this.number_calculation[0] - this.number_calculation[2];
-	}
-	else if(this.number_calculation[1] === '*'){
-		this.plus_result = this.number_calculation[0] * this.number_calculation[2];
-	}
-	else if(this.number_calculation[1] === '/'){
-		this.plus_result = this.number_calculation[0] / this.number_calculation[2];
-	}
-	this.number_calculation[0] = this.plus_result
-}
-number_oprator_event(oprator){
-	this.infix_string.push(oprator)
-  this.number_calculation[1] = oprator;
-  console.log(this.number_calculation[1]);
-}
+// number_event(val:any){
+// 	if(this.flag === 0){
+// 			this.resultcondition_zero = true;
+// 			this.infix_string.push(val)
+// 			console.log(this.infix_string);
+// 			this.number_calculation[0] = val
+// 			console.log(this.number_calculation[0]);
+// 			this.flag = 1;
+// 			console.log(this.flag);
+			
+// 	}
+// 	else if(this.flag === 1){
+// 		this.infix_string.push(val)
+// 		this.number_calculation[2] = val;
+// 		this.resultcondition = true;
+// 		console.log(this.number_calculation[2]);
+// 		this.flag = 0;
+// 		console.log(this.flag);
+// 	}
+// 	else if(this.number_calculation[1] === '+'){
+// 		this.plus_result = this.number_calculation[0] + this.number_calculation[2];
+// 	}
+// 	else if(this.number_calculation[1] === '-'){
+// 		this.plus_result = this.number_calculation[0] - this.number_calculation[2];
+// 	}
+// 	else if(this.number_calculation[1] === '*'){
+// 		this.plus_result = this.number_calculation[0] * this.number_calculation[2];
+// 	}
+// 	else if(this.number_calculation[1] === '/'){
+// 		this.plus_result = this.number_calculation[0] / this.number_calculation[2];
+// 	}
+// 	this.number_calculation[0] = this.plus_result
+// }
+// number_oprator_event(oprator){
+// 	this.infix_string.push(oprator)
+//   this.number_calculation[1] = oprator;
+//   console.log(this.number_calculation[1]);
+// }
 
 // Clear All data
-clear_String(){
-	this.plus_result = 0;
-	this.infix_string = [];
-}
+// clear_String(){
+// 	this.plus_result = 0;
+// 	this.infix_string = [];
+// }
+
+	number_array:any[] = [];
+	number_calculation:any[] = [2]
+
+	number_event(val:any){
+		this.number_array.push(val);
+		console.log(this.number_array);
+		
+		if(this.number_calculation[1] === '+'){
+			this.number_calculation[0] = this.number_array.join("");
+			console.log(this.number_calculation[0]);
+			let a = this.number_calculation[0]
+			console.log(typeof(a));
+			
+			console.log( 2 + (+a));
+		}
+	}
+
+	oprator_event(oprator){
+		this.number_calculation[1] = oprator;
+	}
 }
 
